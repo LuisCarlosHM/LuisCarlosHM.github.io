a92c3c3690041a95b4cf491dbf51104a12b1f601
LuisCarlosHM <95248473+LuisCarlosHM@users.noreply.github.com>
2026-04-29 13:57:55 -0600

    update title to match LinkedIn, add Perl/Bash to skills

--- src/app/components/PersonalInfo.js
+++ src/app/components/PersonalInfo.js
@@ -2,13 +2,12 @@
 import Image from 'next/image';
 import React from 'react';
 import profilePicture from '/public/images/profile.jpeg';
-import TypingEffect from './TypingEffect';
 // Icons
 import { FaHtml5, FaCss3, FaPython, FaNode, FaReact, FaLinkedin, FaGithub, FaAws } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import { TbBrandDjango, TbBrandDocker } from "react-icons/tb";
 import { AiOutlineConsoleSql } from "react-icons/ai";
-import { SiLinux } from "react-icons/si";
+import { SiLinux, SiGnubash, SiPerl } from "react-icons/si";
 
 
 const PersonalInfo = () => {
@@ -24,11 +23,10 @@ const PersonalInfo = () => {
                             quality={100}
                             priority={true}
                         />
-                        
                     </div>
                     <div>
                         <p className="personal_info__name">Luis Hernández</p>
-                        <p className="personal_info__job">Systems Engineer | Cloud & DevOps</p>
+                        <p className="personal_info__job">Software Engineer | System Reliability & Scalable Automation</p>
                     </div>
                 </div> 
                 <div className="personal_info__body">
@@ -50,6 +48,26 @@ const PersonalInfo = () => {
                                 <span className='p_language__name'>Python</span>
                             </div>
                             <div className='p_language'>
+                                <span className='p_language__icon'><SiGnubash/></span>
+                                <span className='p_language__name'>Bash</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><SiPerl/></span>
+                                <span className='p_language__name'>Perl</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><FaAws/></span>
+                                <span className='p_language__name'>AWS</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><SiLinux/></span>
+                                <span className='p_language__name'>Linux</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><TbBrandDocker/></span>
+                                <span className='p_language__name'>Docker</span>
+                            </div>
+                            <div className='p_language'>
                                 <span className='p_language__icon'><IoLogoJavascript/></span>
                                 <span className='p_language__name'>JavaScript</span>
                             </div>
@@ -66,22 +84,10 @@ const PersonalInfo = () => {
                                 <span className='p_language__name'>Django</span>
                             </div>
                             <div className='p_language'>
-                                <span className='p_language__icon'><FaAws/></span>
-                                <span className='p_language__name'>AWS</span>
-                            </div>
-                            <div className='p_language'>
-                                <span className='p_language__icon'><TbBrandDocker/></span>
-                                <span className='p_language__name'>Docker</span>
-                            </div>
-                            <div className='p_language'>
-                                <span className='p_language__icon'><SiLinux/></span>
-                                <span className='p_language__name'>Linux</span>
-                            </div>
-                            <div className='p_language'>
                                 <span className='p_language__icon'><AiOutlineConsoleSql/></span>
                                 <span className='p_language__name'>SQL</span>
                             </div>
-                            <div className = 'p_language'>
+                            <div className='p_language'>
                                 <span className='p_language__icon'><FaHtml5/></span>
                                 <span className='p_language__name'>HTML</span>
                             </div>
@@ -94,7 +100,7 @@ const PersonalInfo = () => {
                         <br/>
                         <hr className='divider'/>
                         <div className='personal_info__social'>
-                            <a className='social_page' href='https://www.linkedin.com/in/luis-hernández-3871aa25a' target='_blank' rel='noopener noreferrer'><FaLinkedin/></a>
+                            <a className='social_page' href='https://www.linkedin.com/in/luis-hern%C3%A1ndez-3871aa25a' target='_blank' rel='noopener noreferrer'><FaLinkedin/></a>
                             <a className='social_page' href='https://github.com/LuisCarlosHM' target='_blank' rel='noopener noreferrer'><FaGithub/></a>
                         </div>
                     </div>
