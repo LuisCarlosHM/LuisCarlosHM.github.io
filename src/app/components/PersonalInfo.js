50cc0d073a51f1792952ac5fee156d226af06468
LuisCarlosHM <95248473+LuisCarlosHM@users.noreply.github.com>
2026-04-29 13:56:49 -0600

    update title, social links, and skills stack

--- src/app/components/PersonalInfo.js
+++ src/app/components/PersonalInfo.js
@@ -4,10 +4,11 @@ import React from 'react';
 import profilePicture from '/public/images/profile.jpeg';
 import TypingEffect from './TypingEffect';
 // Icons
-import { FaHtml5, FaCss3, FaPython, FaNode, FaReact, FaLinkedin, FaGithub, FaHackerrank} from "react-icons/fa";
+import { FaHtml5, FaCss3, FaPython, FaNode, FaReact, FaLinkedin, FaGithub, FaAws } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
-import { TbBrandDjango } from "react-icons/tb";
+import { TbBrandDjango, TbBrandDocker } from "react-icons/tb";
 import { AiOutlineConsoleSql } from "react-icons/ai";
+import { SiLinux } from "react-icons/si";
 
 
 const PersonalInfo = () => {
@@ -27,14 +28,10 @@ const PersonalInfo = () => {
                     </div>
                     <div>
                         <p className="personal_info__name">Luis Hernández</p>
-                        <p className="personal_info__job">Web Developer</p>
-                        {/* <TypingEffect/> */}
+                        <p className="personal_info__job">Systems Engineer | Cloud & DevOps</p>
                     </div>
                 </div> 
                 <div className="personal_info__body">
-                    {/* <p className="personal_info__description">
-                    &quot;Bringing visions to life in pixels and code, one line at a time&quot;
-                    </p> */}
                     <div className="personal_info__skills">
                         <div className="personal_info__languages_list">
                             <div className='language'>
@@ -48,25 +45,13 @@ const PersonalInfo = () => {
                         </div>
                         <hr className='divider'/>
                         <div className='personal_info__programming_languages'>
-                            <div className = 'p_language'>
-                                <span className='p_language__icon'><FaHtml5/></span>
-                                <span className='p_language__name'>HTML</span>
-                            </div>
-                            <div className='p_language'>
-                                <span className='p_language__icon'><FaCss3/></span>
-                                <span className='p_language__name'>CSS</span>
-                            </div>
-                            <div className='p_language'>
-                                <span className='p_language__icon'><IoLogoJavascript/></span>
-                                <span className='p_language__name'>JavaScript</span>
-                            </div>
                             <div className='p_language'>
                                 <span className='p_language__icon'><FaPython/></span>
                                 <span className='p_language__name'>Python</span>
                             </div>
                             <div className='p_language'>
-                                <span className='p_language__icon'><TbBrandDjango/></span>
-                                <span className='p_language__name'>Django</span>
+                                <span className='p_language__icon'><IoLogoJavascript/></span>
+                                <span className='p_language__name'>JavaScript</span>
                             </div>
                             <div className='p_language'>
                                 <span className='p_language__icon'><FaReact/></span>
@@ -77,17 +62,40 @@ const PersonalInfo = () => {
                                 <span className='p_language__name'>Node.js</span>
                             </div>
                             <div className='p_language'>
+                                <span className='p_language__icon'><TbBrandDjango/></span>
+                                <span className='p_language__name'>Django</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><FaAws/></span>
+                                <span className='p_language__name'>AWS</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><TbBrandDocker/></span>
+                                <span className='p_language__name'>Docker</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><SiLinux/></span>
+                                <span className='p_language__name'>Linux</span>
+                            </div>
+                            <div className='p_language'>
                                 <span className='p_language__icon'><AiOutlineConsoleSql/></span>
                                 <span className='p_language__name'>SQL</span>
+                            </div>
+                            <div className = 'p_language'>
+                                <span className='p_language__icon'><FaHtml5/></span>
+                                <span className='p_language__name'>HTML</span>
+                            </div>
+                            <div className='p_language'>
+                                <span className='p_language__icon'><FaCss3/></span>
+                                <span className='p_language__name'>CSS</span>
                             </div>
                         </div>
                         <br/>
                         <br/>
                         <hr className='divider'/>
                         <div className='personal_info__social'>
-                            <a className='social_page' target='_blank'><FaLinkedin/></a>
-                            <a className='social_page' target='_blank'><FaGithub/></a>
-                            <a className='social_page' target='_blank'><FaHackerrank/></a>
+                            <a className='social_page' href='https://www.linkedin.com/in/luis-hernández-3871aa25a' target='_blank' rel='noopener noreferrer'><FaLinkedin/></a>
+                            <a className='social_page' href='https://github.com/LuisCarlosHM' target='_blank' rel='noopener noreferrer'><FaGithub/></a>
                         </div>
                     </div>
                 </div>
